9923e0b1865b5266512e88cd0f47b280d6645420
wiji <wijikurniawan121@gmail.com>
2022-11-30 16:51:51 +0700

    Create QuoteModel.java

---
+++ app/src/main/java/com/si5a/myapplication/Model/QuoteModel.java
@@ -0,0 +1,13 @@
+package com.si5a.myapplication.Model;
+
+public class QuoteModel {
+    private String text, author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
